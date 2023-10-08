5e00cc5efed03a3f042b365f2d654d8584c29a64
vunayang <141714330+vunayang@users.noreply.github.com>
2023-10-07 20:03:34 -0700

    Add files via upload

---
+++ ConsoleApplication88/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        // This is a main method
+        // The main function is an entry function
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+
+
